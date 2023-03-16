c9c55028d7c7e4af0cd6c924c058b5ed43b7375a
Vladimir Grichina <vgrichina@gmail.com>
2023-03-16 00:38:32 -0700

    Refactor code and display system message

--- script.js
+++ script.js
@@ -5,17 +5,44 @@ const chatList = document.getElementById('chat-list');
 const apiUrl = 'https://api.openai.com/v1/chat/completions';
 const apiKey = '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'; // replace with your OpenAI API key
 
+const previousMessages = [{
+  role: 'system',
+  content: `
+Hi! I'm a chatbot that is good at making simple websites for it's users.
+
+I can take any input info from you, ask you questions about missing info and then create a website for you.
+
+I'll start by listing website outline and which information is needed for each section. Then I'll ask you questions about the missing info.
+
+If you don't provide me with answer to a question I'll be creative with info I have from training data.
+
+After that I'll generate website with separate .html, .css and .js files for you.
+I'll represent every file in output like this:
+
+---index.html---
+<html>
+<head>
+  <title>Hello, world!</title>
+</head>
+<body>
+  <h1>Hello, world!</h1>
+</body>
+</html>
+---index.html end---
+  `
+}];
+
 // handle form submission
 chatForm.addEventListener('submit', e => {
   e.preventDefault();
   const userInput = chatInput.value.trim();
   if (userInput !== '') {
-    sendMessage(userInput, 'user');
+    addMessageToList(userInput, 'user');
     chatInput.value = '';
-    getAiResponse(userInput)
+    getAiResponse()
       .then(data => {
         const aiResponse = data.choices[0].message.content.trim();
-        sendMessage(aiResponse, 'ai');
+        addMessageToList(aiResponse, 'assistant');
       })
       .catch(error => console.error('Error:', error));
   }
@@ -37,35 +64,50 @@ const files = [
   }
 ];
 updateFileList();
+updateChatList();
+
+function processAiResponse(text) {
+  // Extract files from AI response
+  const files = text.matchAll(/---([\w.]+)---(.+?)---([\w.]+) end---/gs);
+  // ---file_name.ext--- - start of file
+  // (.+) - file content
+  // ---file_name.ext end--- - end of file
+
+  for (let file of files) {
+    const [, fileName, fileContent] = file;
+    console.log('file name: ', fileName, 'file content: ', fileContent);
+    updateFile(fileName, fileContent);
+  }
+}
 
 // add message to chat list
 function addMessageToList(text, sender) {
-  const messageLi = document.createElement('li');
-  messageLi.classList.add(sender === 'ai' ? 'ai-message' : 'user-message');
-  const parsedText = marked.marked(text.replaceAll(/---([\w.]+)---(.+?)---([\w.]+) end---/gs, (_, fileName) => '`' + fileName + '`'));
-  messageLi.innerHTML = `
-    <div class="message-text">${parsedText}</div>
-  `;
-  chatList.appendChild(messageLi);
-  chatList.scrollTop = chatList.scrollHeight;
+  if (sender === 'assistant') {
+    text = text.replaceAll(/---([\w.]+)---(.+?)---([\w.]+) end---/gs, (_, fileName) => '`' + fileName + '`');
+  }
 
-  if (sender === 'ai') {
-    // Extract files from AI response
-    const files = text.matchAll(/---([\w.]+)---(.+?)---([\w.]+) end---/gs);
-    // ---file_name.ext--- - start of file
-    // (.+) - file content
-    // ---file_name.ext end--- - end of file
+  previousMessages.push({
+    role: sender,
+    content: text
+  });
 
-    for (let file of files) {
-      console.log('file: ', file, Array.from(file));
-      const [, fileName, fileContent] = file;
-      console.log('file name: ', fileName, 'file content: ', fileContent);
-      updateFile(fileName, fileContent);
-    }
+  updateChatList();
 
+  if (sender === 'assistant') {
+    processAiResponse(text);
+    updateChatList();
     updateFileList();
     previewWebsite();
   }
+}
+
+function updateChatList() {
+  chatList.innerHTML = previousMessages.map(message => `
+    <li class="${message.role === 'assistant' || message.role === 'system' ? 'ai-message' : 'user-message'}">
+      <div class="message-text">${marked.marked(message.content)}</div>
+    </li>
+  `).join('\n');
+  chatList.scrollTop = chatList.scrollHeight;
 }
 
 function updateFile(name, content) {
@@ -136,41 +178,15 @@ function previewWebsite() {
   document.querySelector('.website-preview').srcdoc = replaceUrls(index.content, files);
 }
 
-// send user message to chat list
-function sendMessage(text, sender) {
-  addMessageToList(text, sender);
-}
-
-// get AI response and add to chat list
 function getAiResponse(userInput) {
-  const previousMessages = chatList.querySelectorAll('.user-message, .ai-message');
-  const messages = [{
-      role: 'system',
-      content: `
-        Hi! I'm a chatbot that is good at making simple websites for it's users.
+  const messages = [...previousMessages];
 
-        I can take any input info from you, ask you questions about missing info and then create a website for you.
-
-        I'll start by listing website outline and which information is needed for each section. Then I'll ask you questions about the missing info.
-
-        If you don't provide me with answer to a question I'll be creative with info I have from training data.
-
-        After that I'll generate website with separate .html, .css and .js files for you.
-        I'll represent every file in output like this:
-
-        ---file_name.ext---
-        [actual file content goes here]
-        ---file_name.ext end---
-      `
-  }].concat(Array.from(previousMessages).map(message => ({
-    role: message.classList.contains('user-message') ? 'user' : 'assistant',
-    content: message.querySelector('.message-text').textContent
-  })));
-
-  messages.push({
-    role: 'user',
-    content: userInput
-  });
+  if (userInput) {
+    messages.push({
+      role: 'user',
+      content: userInput
+    });
+  }
 
   const requestBody = {
     model: 'gpt-3.5-turbo',
